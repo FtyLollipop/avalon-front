2a790311101bac755cdce1e3c84f8fb763acc616
FtyLollipop <ftylollipop@outlook.com>
2020-09-10 20:19:56 +0800

    update

---
+++ js/mouse_tip.js
@@ -0,0 +1,34 @@
+function display_avatar_number_mouse_tip(div){
+
+}
+
+function hide_avatar_number_mouse_tip(div){
+
+}
+
+
+function display_avatar_me_mouse_tip(div){
+
+}
+
+function hide_avatar_me_mouse_tip(div){
+
+}
+
+
+function display_avatar_leader_mouse_tip(div){
+
+}
+
+function hide_avatar_leader_mouse_tip(div){
+
+}
+
+
+function display_avatar_character_mouse_tip(div){
+
+}
+
+function hide_avatar_character_mouse_tip(div){
+
+}
