9b22a57c79c780af5b6c2d07088e8e427dacccc2
FtyLollipop <ftylollipop@outlook.com>
2020-09-11 22:00:47 +0800

    update

--- js/ready_run.js
+++ js/ready_run.js
@@ -10,12 +10,33 @@ $(document).ready(function(){
             $("#ui-box").css("width",screenWidth);
             $("#ui-box").css("height",screenWidth*0.75);
         }
-    },100);
+    },50);
 });
 
 $(document).mousemove(function (e) {
-    let xPos = parseInt(e.clientX+10) + "px";
-    let yPos = parseInt(e.clientY+10) + "px";
-    $("#mouse-tip").css("left", xPos);
-    $("#mouse-tip").css("top", yPos);
+    let xPos = parseInt(e.clientX+10);
+    let yPos = parseInt(e.clientY+10);
+    if(screenHeight*0.6<yPos&&screenWidth*0.6<xPos){
+        $("#mouse-tip").addClass("mouse-tip-xy-pos");
+        yPos-=12;
+        xPos-=12;
+    } else{
+        $("#mouse-tip").removeClass("mouse-tip-xy-pos");
+        if(screenHeight*0.6<yPos){
+            $("#mouse-tip").addClass("mouse-tip-y-pos");
+            yPos-=12;
+        } else{
+            $("#mouse-tip").removeClass("mouse-tip-y-pos");
+        }
+
+        if(screenWidth*0.6<xPos){
+            $("#mouse-tip").addClass("mouse-tip-x-pos");
+            xPos-=12;
+        } else{
+            $("#mouse-tip").removeClass("mouse-tip-x-pos");
+        }
+    }
+
+    $("#mouse-tip").css("left", xPos+"px");
+    $("#mouse-tip").css("top", yPos+"px");
 });
